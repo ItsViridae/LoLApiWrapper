f84c2bbb8f3e2e42e7b0e17c137c561727f045a3
Scott Comeaux <viridaecode@gmail.com>
2020-06-17 22:47:31 -0500

    JupyterNotebook - Data

--- FlaskAPI/PullLoLDataAssets.py
+++ FlaskAPI/PullLoLDataAssets.py
@@ -5,10 +5,12 @@ def getCurrentVersion():
     versionResponse = requests.get("https://ddragon.leagueoflegends.com/api/versions.json")
     version_patch_RawData = versionResponse.json()
     currentVersion = version_patch_RawData[0]
+    print(currentVersion)
     return currentVersion
 
 #champions, items, summoner_spells, spells
-def GetDDragonData_Champions(version):
+def GetDDragonData_Champions():
+    version = getCurrentVersion()
     #Champions Data
     response = requests.get("http://ddragon.leagueoflegends.com/cdn/"+version+"/data/en_US/champion.json")
     allChampionRawData = json.loads(response.text)
@@ -17,19 +19,25 @@ def GetDDragonData_Champions(version):
     for key,champion in allChampionRawData['data'].items():
         ChampionIdToName[int(champion['key'])] = champion['name']
     print(ChampionIdToName)
+    return ChampionIdToName
 
-def GetDDragonData_Items(version):
+def GetDDragonData_Items():
+    version = getCurrentVersion()
     response = requests.get("http://ddragon.leagueoflegends.com/cdn/"+version+"/data/en_US/item.json")
     allItemsRawData = json.loads(response.text)
+    QuickPrinter(allItemsRawData)
 
     #Items Data
     ItemIdToName = {}
     for key,item in allItemsRawData['data'].items():
         ItemIdToName[int(key)] = item['name']
+    print(ItemToToName)
+    return ItemIdToName
 
 def QuickPrinter(String_to_Print):
     print(json.dumps(String_to_Print, indent=4, sort_keys=True))
 
+#main()
 version = getCurrentVersion()
-#GetDDragonData_Champions(version)
-#GetDDragonData_Items(version)
+GetDDragonData_Champions()
+GetDDragonData_Items()
